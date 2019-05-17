f82e1805848b7065c44bc52397d4482d702ac00a
Christopher Stock <christopher.stock@mayflower.de>
2019-05-18 00:33:30 +0200

    Heavy Kotlin evaluation :D

--- src/app.kt
+++ src/app.kt
@@ -1,5 +1,33 @@
-
-    fun main( args: Array<String> )
+/*
+    fun util.main( args: Array<String> )
     {
-        println( "Hello World!" )
+        println( "Hello Kotlin with Args" )
+    }
+*/
+    const val `My Identifier 1$` :String = "Test Identifier"
+
+    fun main()
+    {
+        println( "Hello Kotlin from util.main" )
+
+        var number = 42
+        var message = "Hello"
+
+        var number2:Int = 42
+        var message2:String = "Hello"
+
+        // constants
+        val NUMBER3:Int = 42
+        val MESSAGE3:String = "Hello"
+
+        println( number2 )
+        println( MESSAGE3 )
+
+        val x: String
+        x = "My Value X"
+
+        println( `My Identifier 1$` )
+
+
+
     }
---
+++ src/util/test.kt
@@ -0,0 +1,13 @@
+
+    package util
+
+    const val TEST_CONST :String = "TEST CONST 2"
+
+    fun testMain()
+    {
+        println( "Test Main" )
+
+
+
+
+    }
