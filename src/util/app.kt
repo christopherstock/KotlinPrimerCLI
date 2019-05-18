8221e00a14e06fd4d060589a5cd41ccc4fb359a7
Christopher Stock <christopher.stock@mayflower.de>
2019-05-18 12:19:23 +0200

    Minor tryouts.

--- src/util/app.kt
+++ src/util/app.kt
@@ -1,12 +1,77 @@
 
     package util
 
-    const val `My Identifier 1$` :String = "Test Identifier"
-    const val TEST_APP :String = "Test App"
+    import javax.swing.JFrame
+
+    private const val `My Identifier 1$` :String = "Test Identifier"
+    private const val TEST_APP :String = "Test App"
 
     fun main()
     // fun main( args: Array<String> )
     {
+        var frame = JFrame()
+        frame.setTitle( "Mein Fenster" )
+        frame.setVisible( true )
+
+
+        var t : HashMap<String, String> = HashMap<String, String>()
+
+        // var x = t.get( "iwas" )
+
+
+
+        var matthias = 7
+
+        var benjamin :CharSequence = "test"
+
+        var benjamin2 = 7.3
+
+
+//        var test6 : MutableList<String> = MutableList()
+
+//        test6 = "test7"
+
+val strings  : MutableList<String> = mutableListOf("Anne", "Karen", "Peter")
+val strings2 : List<String> = listOf("Anne", "Karen", "Peter")
+
+strings.dropLast(  0 )
+strings.add( "test" )
+
+strings2.dropLast( 0 )
+
+
+ for ( i in 0..10 )
+
+// for ( val i = 0; i < 10; ++i )
+{
+    println( i )
+}
+
+
+
+var testA :String  = ""
+var testB :String? = null
+
+testA = "sddopskosd"
+
+testB = null
+testB = "dssdds"
+//testB = null
+
+    println( "Anzahl Zeichen: " + testB.substring( 0..5 ) )
+
+
+
+
+
+// benjamin += java.lang.String.valueOf( 1 )
+// benjamin = "test2"
+
+
+// println( benjamin )
+
+
+
         println( "Hello Kotlin from util.util.main" )
 
         var number = 42
@@ -18,6 +83,8 @@
         // constants
         val NUMBER3:Int = 42
         val MESSAGE3:String = "Hello"
+
+        number2 = 7
 
         println( number2 )
         println( MESSAGE3 )
--- src/util/test.kt
+++ src/util/test.kt
@@ -8,16 +8,25 @@
 
     class Test
     {
-        fun testMain()
+/*
+        fun JFrame()
+        {
+        }
+*/
+        fun testMain() : Int
         {
             var test = HashMap<Int,Int>()
+
+// var frame :JFrame = JFrame()
 
             var test2 = JFrame()
             test2.show()
 
+println( "1" ); println( "2" )
+
             println( "Test Main >> size: [" + test.size + "]" )
 
-            println( TEST_APP )
+            // println( TEST_APP )
 
 
 val strings = listOf("Anne", "Karen", "Peter") // List<String>
@@ -25,6 +34,6 @@ val map = mapOf("a" to 1, "b" to 2, "c" to 3)  // Map<String, Int>
 val set = setOf("a", "b", "c")                 // Set<String>
 
 
-
+            return 5
         }
     }
